edf0fd4700e36616b3da0b79bfa240948b2d5d8a
Vishwas Charan <vishwascharan11@gmail.com>
2026-06-30 21:58:21 +0530

    fixed prod errors

--- src/components/ui/RotatingText.tsx
+++ src/components/ui/RotatingText.tsx
@@ -64,25 +64,25 @@ const RotatingText = forwardRef<any, RotatingTextProps>((props, ref) => {
     const currentText = texts[currentTextIndex];
     if (splitBy === 'characters') {
       const words = currentText.split(' ');
-      return words.map((word, i) => ({
+      return words.map((word: string, i: number) => ({
         characters: splitIntoCharacters(word),
         needsSpace: i !== words.length - 1
       }));
     }
     if (splitBy === 'words') {
-      return currentText.split(' ').map((word, i, arr) => ({
+      return currentText.split(' ').map((word: string, i: number, arr: string[]) => ({
         characters: [word],
         needsSpace: i !== arr.length - 1
       }));
     }
     if (splitBy === 'lines') {
-      return currentText.split('\n').map((line, i, arr) => ({
+      return currentText.split('\n').map((line: string, i: number, arr: string[]) => ({
         characters: [line],
         needsSpace: i !== arr.length - 1
       }));
     }
 
-    return currentText.split(splitBy).map((part, i, arr) => ({
+    return currentText.split(splitBy).map((part: string, i: number, arr: string[]) => ({
       characters: [part],
       needsSpace: i !== arr.length - 1
     }));
@@ -176,11 +176,11 @@ const RotatingText = forwardRef<any, RotatingTextProps>((props, ref) => {
           layout
           aria-hidden="true"
         >
-          {elements.map((wordObj, wordIndex, array) => {
-            const previousCharsCount = array.slice(0, wordIndex).reduce((sum, word) => sum + word.characters.length, 0);
+          {elements.map((wordObj: { characters: string[], needsSpace: boolean }, wordIndex: number, array: { characters: string[], needsSpace: boolean }[]) => {
+            const previousCharsCount = array.slice(0, wordIndex).reduce((sum: number, word: { characters: string[], needsSpace: boolean }) => sum + word.characters.length, 0);
             return (
               <span key={wordIndex} className={cn('inline-flex', splitLevelClassName)}>
-                {wordObj.characters.map((char, charIndex) => (
+                {wordObj.characters.map((char: string, charIndex: number) => (
                   <motion.span
                     key={charIndex}
                     initial={initial}
@@ -190,7 +190,7 @@ const RotatingText = forwardRef<any, RotatingTextProps>((props, ref) => {
                       ...transition,
                       delay: getStaggerDelay(
                         previousCharsCount + charIndex,
-                        array.reduce((sum, word) => sum + word.characters.length, 0)
+                        array.reduce((sum: number, word: { characters: string[], needsSpace: boolean }) => sum + word.characters.length, 0)
                       )
                     }}
                     className={cn('inline-block', elementLevelClassName)}
